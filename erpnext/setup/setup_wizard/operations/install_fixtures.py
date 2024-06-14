1218b08d02f1579c1653f0b4e8d2c14e6fde0163
Ekasa Technology <111553452+EkasaTech@users.noreply.github.com>
2024-06-14 10:53:37 +0700

    Change Default UOM

--- erpnext/setup/setup_wizard/operations/install_fixtures.py
+++ erpnext/setup/setup_wizard/operations/install_fixtures.py
@@ -481,7 +481,7 @@ def update_stock_settings():
 	stock_settings.default_warehouse = frappe.db.get_value(
 		"Warehouse", {"warehouse_name": _("Stores")}
 	)
-	stock_settings.stock_uom = _("Nos")
+	stock_settings.stock_uom = _("Unit")
 	stock_settings.auto_indent = 1
 	stock_settings.auto_insert_price_list_rate_if_missing = 1
 	stock_settings.automatically_set_serial_nos_based_on_fifo = 1
