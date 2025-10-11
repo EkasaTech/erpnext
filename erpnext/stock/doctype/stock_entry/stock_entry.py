f5d50786e9719ff37b8175fc5ed5d433409f8e2f
Oscar <oscarutomo@gmail.com>
2025-10-11 11:28:58 +0700

    stock entry parent remark

--- erpnext/stock/doctype/stock_entry/stock_entry.py
+++ erpnext/stock/doctype/stock_entry/stock_entry.py
@@ -1234,6 +1234,9 @@ class StockEntry(StockController):
 				sle = self.get_sl_entries(
 					d, {"warehouse": cstr(d.s_warehouse), "actual_qty": -flt(d.transfer_qty), "incoming_rate": 0}
 				)
+				# 👉 Add the remark from parent document
+				sle.parent_remark = self.remarks or ""
+
 				if cstr(d.t_warehouse):
 					sle.dependant_sle_voucher_detail_no = d.name
 				elif finished_item_row and (
@@ -1254,6 +1257,8 @@ class StockEntry(StockController):
 						"incoming_rate": flt(d.valuation_rate),
 					},
 				)
+				# 👉 Add the remark from parent document
+				sle.parent_remark = self.remarks or ""
 				if cstr(d.s_warehouse) or (finished_item_row and d.name == finished_item_row.name):
 					sle.recalculate_rate = 1
 
